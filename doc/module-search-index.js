2a778bddf1277afb07b603aee323c10f69348cbd
Caroidk <c.p.t.cortes16@gmail.com>
2022-10-31 17:29:00 -0400

    javadoc

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Taller_02"}];updateSearchResults();
---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Taller_02","l":"dominio"},{"m":"Taller_02","l":"logica"}];updateSearchResults();
